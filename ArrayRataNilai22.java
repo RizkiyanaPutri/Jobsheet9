e65ec52ec235d76160fea9c2a2e88e2552d10f8a
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-11-07 21:09:39 +0700

    Percobaan 3: modifikasi no.2

--- ArrayRataNilai22.java
+++ ArrayRataNilai22.java
@@ -2,24 +2,32 @@ import java.util.Scanner;
 public class ArrayRataNilai22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-        int jmlLulus = 0;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jmlMhs];
+        int jmlLulus = 0, jmlTidakLulus = 0;
+        double totalLulus = 0, totalTidakLulus = 0;
+        double avgNilaiLulus, avgNilaiTidakLulus;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-            if (nilaiMhs[i] > 70) {
+            if (nilaiMhs[i] > 70) {  
+                totalLulus += nilaiMhs[i];
                 jmlLulus++;
+            }else if (nilaiMhs[i] <= 70 ) {
+                totalTidakLulus += nilaiMhs[i];
+                jmlTidakLulus++;
             }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-        System.out.println("Jumlah mahasiswa lulus = "+ jmlLulus);
+
+        avgNilaiLulus = totalLulus / jmlLulus;
+        avgNilaiTidakLulus = totalTidakLulus / jmlTidakLulus;
+        System.out.println("Rata-rata nilai lulus = "+avgNilaiLulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+avgNilaiTidakLulus);
         sc.close();
     }
 }
